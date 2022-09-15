7520ec93ebab043cad44957957d71c8f3771d7d8
Sumit Gupta <summitgupta07@gmail.com>
2022-09-15 12:37:43 +0530

    apply @in global

---
+++ utils/data.js
@@ -0,0 +1,55 @@
+const data = {
+    products : [
+        {
+            id:1,
+            name:"free shirt",
+            slug:"free shirt",
+            image:"free shirt",
+            rating:"free shirt",
+            brand:"free shirt",
+        },
+        {
+            id:2,
+            name:"free shirt",
+            slug:"free shirt",
+            image:"free shirt",
+            rating:"free shirt",
+            brand:"free shirt",
+        },
+        {
+            id:3,
+            name:"free shirt",
+            slug:"free shirt",
+            image:"free shirt",
+            rating:"free shirt",
+            brand:"free shirt",
+        },
+        {
+            id:6,
+            name:"free shirt",
+            slug:"free shirt",
+            image:"free shirt",
+            rating:"free shirt",
+            brand:"free shirt",
+        },
+        {
+            id:4,
+            name:"free shirt",
+            slug:"free shirt",
+            image:"free shirt",
+            rating:"free shirt",
+            brand:"free shirt",
+        },
+        {
+            id:5,
+            name:"free shirt",
+            slug:"free shirt",
+            image:"free shirt",
+            rating:"free shirt",
+            brand:"free shirt",
+        },
+    ]
+}
+
+
+export default data;
